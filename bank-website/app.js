29dd9b651c1484a76267efb1553a7f61a1694203
Dave Smith <dsmith@hirevue.com>
2016-09-27 01:14:38 +0100

    Dress up the UI a bit

--- bank-website/app.js
+++ bank-website/app.js
@@ -1,3 +1,4 @@
+// vim: ts=2:sw=2
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
@@ -5,36 +6,47 @@ var https = require('https');
 var fs = require('fs');
 
 // CSRF Protection:
-var cookieParser = require('cookie-parser')
-var csrf = require('csurf')
+//var cookieParser = require('cookie-parser')
+//var csrf = require('csurf')
 
 var app = express();
 
-app.use(session({secret: 'secret!', saveUninitialized: true, resave: false, cookie: {maxAge: 3*24*60*60*1000}}));
+app.use(session({secret: 'secret!', saveUninitialized: true, resave: false, cookie: {maxAge: 7*24*60*60*1000}}));
 app.use(bodyParser.urlencoded({extended: false}));
 
 // CSRF Protection:
-app.use(cookieParser());
-app.use(csrf({cookie: true}));
+//app.use(cookieParser());
+//app.use(csrf({cookie: true}));
 
 app.post('/money/transfer', function(req, res) {
+  // This is a pretend money transfer endpoint
+  var msg;
   if (req.session.username) {
-    var msg = 'Money transfer from account: ' + req.session.username + ', to account: ' + req.body.account + ', amount: ' + req.body.amount;
+    msg = 'Success!';
+    msg += ' Money transferred from account "' + req.session.username + '"';
+    msg += ' to account ' + req.body.account;
+    msg += ', amount: ' + req.body.amount;
   } else {
-    var msg = 'Could not transfer money: You must be logged in';
+    msg = 'Could not transfer money: You must be logged in';
   }
-  console.log(msg);
   res.send(msg);
 });
 
 app.get('/', function(req, res) {
   var output = '<h1>Welcome to Very Real Bank</h1>';
   if (req.session.username !== undefined) {
-    output += 'You are logged in as "' + req.session.username + '"';
+    output += 'You are logged in as "' + req.session.username + '"<br/><br/>';
+    output += '<h3>Transfer money:</h3>';
+    output += '<form method="POST" action="/money/transfer">';
+    output += '<input type="hidden" name="_csrf" value="' + (req.csrfToken ? req.csrfToken() : '')+ '"/>';
+    output += 'To account:<br/><input type="text" name="account" value=""/><br/>';
+    output += 'Amount:<br/><input type="text" name="amount" value=""/><br/>';
+    output += '<input type="submit" name="submit" value="Transfer"/><br/>';
+    output += '</form>';
   } else {
     output += 'You are not logged in. <a href="/login">Log in now</a>.';
   }
-  output += '<br/><br/>Current time: ' + new Date();
+  output += '<br/><br/><i>Current time: ' + new Date() + '</i>';
   res.send(output);
 });
 
@@ -54,6 +66,9 @@ app.post('/login', function(req, res) {
   res.redirect('/');
 });
 
+
+
+// Server config:
 var options = {
   key: fs.readFileSync('server.key'),
   cert: fs.readFileSync('server.crt'),
